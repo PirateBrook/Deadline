06b90ae88f89b51a47db4a5f102d6e4c514c0ed3
piratebrook <779389629@qq.com>
2017-11-21 21:03:30 +0800

    temp

--- app/src/main/java/com/piratebrook/deadline/MainActivity.java
+++ app/src/main/java/com/piratebrook/deadline/MainActivity.java
@@ -9,7 +9,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.piratebrook.sdk.component.ErrorViewActivity;
+
+public class MainActivity extends ErrorViewActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
